32170c3b92d96070dc0e9fa591010e09a3f81737
Manoel Tesar <manoel.tesar@gmail.com>
2023-01-08 23:34:19 +0100

    Made some changes to config and plugins

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Creating directory structure in ~/.config/nvim/"
+mkdir -p ~/.config/nvim/after/plugin/ ~/.config/nvim/colors/ ~/.config/nvim/lua/
+
+echo "Creating links to the config files"
+ln ./after/plugin/* ~/.config/nvim/after/plugin/
+ln ./lua/* ~/.config/nvim/lua/
+ln ./colors/PaperColor.vim ~/.config/nvim/colors/
+ln ./init.lua ~/.config/nvim/init.lua
+
+echo "Done!"
